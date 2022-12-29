746746842dd362347087085c06cd980075d36c68
lgm1995 <rjsahr5045@gmail.com>
2022-12-29 17:48:28 +0900

    200번 버스 샘플 데이터 받기 성공

--- src/main/java/com/lgm/publicdata/controller/MainController.java
+++ src/main/java/com/lgm/publicdata/controller/MainController.java
@@ -1,23 +1,62 @@
 package com.lgm.publicdata.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class MainController {
+    //버스 노선 조회용 키 (버스 번호를 확인하기 위함) [경기도_버스노선 조회]
+    private final String key1
+            = "8nVvnCaHdPMQ1FyiBt%2FGDPqoVvFrRLfmbD42Cw9IGEN%2By7ZEaAYEvklEovHXVs5ac7V8IuvU7LWPUUoh4DtFRQ%3D%3D";
+//            = "8nVvnCaHdPMQ1FyiBt/GDPqoVvFrRLfmbD42Cw9IGEN+y7ZEaAYEvklEovHXVs5ac7V8IuvU7LWPUUoh4DtFRQ==";
+    //버스 도착 정보 조회용 키 (정류소에 도착예정 버스 정보를 확인하기 위함) [경기도_버스도착정보 조회]
+    private final String key2
+            = "8nVvnCaHdPMQ1FyiBt%2FGDPqoVvFrRLfmbD42Cw9IGEN%2By7ZEaAYEvklEovHXVs5ac7V8IuvU7LWPUUoh4DtFRQ%3D%3D";
+    // 버스 정류소 조회용 키 (정류소 ID를 확인하기 위함) [경기도_정류소 조회]
+    private final String key3
+            = "8nVvnCaHdPMQ1FyiBt%2FGDPqoVvFrRLfmbD42Cw9IGEN%2By7ZEaAYEvklEovHXVs5ac7V8IuvU7LWPUUoh4DtFRQ%3D%3D";
+
+    // 버스 노선 조회 url
+    private final String baseUrl1
+            = "http://apis.data.go.kr/6410000/busrouteservice";
+    // 버스 도착 정보 url
+    private final String baseUrl2
+            = "http://apis.data.go.kr/6410000/busarrivalservice";
+    // 버스 정류소 조회 url
+    private final String baseUrl3
+            = "http://apis.data.go.kr/6410000/busstationservice";
+
     @GetMapping("/ip")
-    public ResponseEntity<String> findIp(HttpServletRequest req) {
-        String ip = req.getHeader("X-Forwarded-For");
-        if (ip == null) {
-            ip = req.getRemoteAddr();
-        }
-        return ResponseEntity.ok(ip);
+    public ResponseEntity<String> getClientIp(HttpServletRequest req) {
+        return ResponseEntity.ok(req.getRemoteAddr());
     }
+
+    @GetMapping("/public")
+    public ResponseEntity<String> getPublic(HttpServletRequest req) throws IOException, URISyntaxException {
+        URI uri = new URI("http://apis.data.go.kr/6410000/busrouteservice/getBusRouteInfoItem?serviceKey=" + key1 + "&routeId=229000028");
+
+        // Spring에서 Rest 방식 API를 호출할 수 있는 내장 클래스
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<String> response = restTemplate.getForEntity(uri, String.class);
+        return response;
+    }
+
 }
--- src/main/java/com/lgm/publicdata/entity/daily.java
+++ src/main/java/com/lgm/publicdata/entity/daily.java
@@ -1,4 +1,23 @@
 package com.lgm.publicdata.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@NoArgsConstructor
+@Entity
 public class daily {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String ip;
+
+    private Long count;
+
+    private Date first_vi;
+
 }
