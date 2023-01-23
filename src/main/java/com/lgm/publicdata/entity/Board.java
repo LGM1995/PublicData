0c22f7a572a4382a4decb584e8169347a2cc8b6c
lgm1995 <rjsahr5045@gmail.com>
2023-01-23 20:21:30 +0900

    주석 추가

--- src/main/java/com/lgm/publicdata/entity/Board.java
+++ src/main/java/com/lgm/publicdata/entity/Board.java
@@ -2,10 +2,12 @@ package com.lgm.publicdata.entity;
 
 import lombok.Data;
 
+// @Data 를 사용하면 겟터와 셋터를 지정할 필요 없음
 @Data
 public class Board {
 
     private Long id;
+
     private String title;
     private String content;
 }
