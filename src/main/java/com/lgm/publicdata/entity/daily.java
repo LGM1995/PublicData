714fd4eb01e25f5bb210f371c2da035e821a509e
lgm1995 <rjsahr5045@gmail.com>
2023-01-24 15:05:11 +0900

    주석 추가

--- src/main/java/com/lgm/publicdata/entity/daily.java
+++ src/main/java/com/lgm/publicdata/entity/daily.java
@@ -10,6 +10,7 @@ import java.util.Date;
 @NoArgsConstructor
 @Entity
 public class daily {
+    // JPA를 사용하면 인덱스 값을 생성하기 위해 ID 값을 지정해줘야 한다.
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
